6e4410eb85f7872687d06eef1611540174489e15
Tushar Bharambe <tusharbharambe@Tushars-MacBook-Air.local>
2025-08-25 13:17:03 +0530

    initiate the channel model to add the image and replace the image name and delete file also to

--- Djchat/Server/models.py
+++ Djchat/Server/models.py
@@ -4,7 +4,11 @@ from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
 
+def Channel_icon_uplpad_path(instance, filename):
+    return f"Channel/{instance.id}/Channel_icon/{filename}"
 
+def Channel_banner_upload_path(instance, filename):
+    return f"Channel/{instance.id}/Channel_banner/{filename}"
 
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
@@ -47,6 +51,23 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="channel_owner")
     topic = models.CharField(max_length=150, blank=True, null=True)
     server = models.ForeignKey(Server,on_delete=models.CASCADE,related_name="channel_server")
-
+    banner = models.ImageField(upload_to=Channel_banner_upload_path,null=True, blank=True)
+    icon = models.ImageField(upload_to=Channel_icon_uplpad_path,null=True,blank=True)
+    
+    
+    def save(self,*args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Channel, id = self.id) 
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Channel,self).save(*args, **kwargs)
+    
+    @receiver(models.signals.pre_delete, sender = 'Server.Channel')
+    def Channel_delete_files(sender, instance, *args, **kwargs):
+        for field in instance._meta.fields:
+            if field.name =="icon" or field.name == 'banner':
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
     def __str__(self):
         return self.name
